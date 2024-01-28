b2521d48cd2ceeeb17616c1a2216dc402568bd46
gaouimounir <mounir.gaoui@outlook.com>
2024-01-28 21:46:54 +0100

    feat: :wheelchair: ajout d'un menu d'accueil

--- convertisseur.py
+++ convertisseur.py
@@ -1,1 +1,6 @@
 print("Ce programme vous permet d'eefectuer des conversions d'unités.")
+print("Choisissez parmi celles-ci:")
+print("1 - Pouces vers cm")
+print("2 - cm vers pouces")
+
+choice = input("Votre choix: ")

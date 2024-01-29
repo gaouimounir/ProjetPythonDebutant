dd6efcfa7648b1d666d5d19fb785b43226ea45ab
gaouimounir <mounir.gaoui@outlook.com>
2024-01-29 15:59:45 +0100

    feat: :zap: conversion de pouces vers cm

--- convertisseur.py
+++ convertisseur.py
@@ -3,4 +3,10 @@ print("Choisissez parmi celles-ci:")
 print("1 - Pouces vers cm")
 print("2 - cm vers pouces")
 
-choice = input("Votre choix: ")
+choice = input("Votre choix (1 ou 2): ")
+
+if choice == "1":
+    valeurStrPouces = input("Conversion Pouces -> cm. Entrez la valeur en pouces: ")
+    valeurPouces = float(valeurStrPouces)
+    valeurCm = round(valeurPouces * 2.54, 2)
+    print(f"Resultat de la conversion : {valeurPouces} pouces = {valeurCm} cm")

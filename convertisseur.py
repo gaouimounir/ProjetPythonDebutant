fa73f4286970a06a2a7932a92a5844cbc3667522
gaouimounir <mounir.gaoui@outlook.com>
2024-01-29 17:16:25 +0100

    feat: :fire: ajout d'une boucle pour revenir au menu

--- convertisseur.py
+++ convertisseur.py
@@ -11,17 +11,21 @@ def EffectuerConversion(unit1, unit2, facteur):
     valeurConvertie = round(valeurFloat * facteur, 2)
     print(f"Resultat de la conversion : {valeurFloat} {unit1} = {valeurConvertie} {unit2}")
 
+while True:
+    print("Ce programme vous permet d'effectuer des conversions d'unités.")
+    print("Choisissez parmi celles-ci:")
+    print("1 - Pouces vers cm")
+    print("2 - cm vers pouces")
 
-print("Ce programme vous permet d'effectuer des conversions d'unités.")
-print("Choisissez parmi celles-ci:")
-print("1 - Pouces vers cm")
-print("2 - cm vers pouces")
+    choice = input("Votre choix (1 ou 2): ")
 
-choice = input("Votre choix (1 ou 2): ")
+    if choice == "1" or choice == "2":
+        break
+    print("Veuillez choisir entre 1 et 2")
 
-if choice == "1":
-    EffectuerConversion("pouces", "cm", 2.54)
+    if choice == "1":
+        EffectuerConversion("pouces", "cm", 2.54)
 
-if choice == "2":
-    EffectuerConversion("cm", "pouces", 0.393701)
+    if choice == "2":
+        EffectuerConversion("cm", "pouces", 0.393701)
 
